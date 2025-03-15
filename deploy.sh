ce68349ac7b5d8b5aa4e22e87f718933137c4a9c
JakubSieron-Git <thyned@gmail.com>
2025-03-15 04:22:52 +0000

    14

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
-
-
 sudo apt update && sudo apt install -y nodejs npm
-
-
 sudo npm install -g pm2
-pm2 stop example _app
-cd ExampleApplication/
-npm install
-pm2 start ./bin/www --name example_app
+if [ -d "ExampleApplication" ]; then
+  rm -rf ExampleApplication
+fi
+
+git clone https://github.com/JakubSieron/ExampleApplication.git || exit 1
+cd ExampleApplication || exit 1
+
+npm install || exit 1
+chmod +x ./bin/www || true
+
+pm2 list | grep -q example_app
+if [ $? -eq 0 ]; then
+  pm2 stop example_app
+  pm2 delete example_app
+fi
+
+pm2 start ./bin/www --name example_app -f || exit 1
+pm2 save
+pm2 startup
